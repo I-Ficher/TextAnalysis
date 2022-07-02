96a328b3fe5b0547b7b1440b167776d5d4890be6
I-Ficher <67866033+I-Ficher@users.noreply.github.com>
2022-07-02 21:10:27 +0300

    MainText

---
+++ TextAnalyzer/Form1.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace TextAnalyzer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.richTextBox1.Location = new System.Drawing.Point(0, 28);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(587, 422);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(819, 28);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openFileToolStripMenuItem,
+            this.saveFileToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(47, 24);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openFileToolStripMenuItem
+            // 
+            this.openFileToolStripMenuItem.Name = "openFileToolStripMenuItem";
+            this.openFileToolStripMenuItem.Size = new System.Drawing.Size(153, 26);
+            this.openFileToolStripMenuItem.Text = "open file";
+            this.openFileToolStripMenuItem.Click += new System.EventHandler(this.openFileToolStripMenuItem_Click);
+            // 
+            // saveFileToolStripMenuItem
+            // 
+            this.saveFileToolStripMenuItem.Name = "saveFileToolStripMenuItem";
+            this.saveFileToolStripMenuItem.Size = new System.Drawing.Size(153, 26);
+            this.saveFileToolStripMenuItem.Text = "save file";
+            this.saveFileToolStripMenuItem.Click += new System.EventHandler(this.saveFileToolStripMenuItem_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.label1.Location = new System.Drawing.Point(587, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(117, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "кол-во символов";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(587, 58);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(85, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "кол-во слов";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(587, 89);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(106, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "длинное слово";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(587, 115);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(98, 17);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "частые слова";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(587, 396);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(213, 17);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "процентное соотношение букв";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(590, 331);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(198, 22);
+            this.textBox1.TabIndex = 7;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(590, 359);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Найти";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(696, 359);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(92, 23);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Очистить";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(819, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openFileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveFileToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ TextAnalyzer/Form1.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace TextAnalyzer
+{
+    public partial class Form1 : Form
+    {
+        public bool change;
+        Timer timer = new Timer();
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void saveFileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog SaveFile = new SaveFileDialog();
+            SaveFile.DefaultExt = "*.txt";
+            SaveFile.Filter = "Text Files(*.txt)|*.txt";
+            if(SaveFile.ShowDialog()==System.Windows.Forms.DialogResult.OK && SaveFile.FileName.Length > 0)
+            {
+                using (StreamWriter sw = new StreamWriter(SaveFile.FileName, true))
+                {
+                    sw.WriteLine(richTextBox1.Text);
+                    sw.Close();
+                }
+            }
+        }
+
+        private void openFileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            
+            OpenFileDialog openFile = new OpenFileDialog();
+            if (openFile.ShowDialog() == DialogResult.OK)
+            {
+                richTextBox1.Text = File.ReadAllText(openFile.FileName);
+            }
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+            if (richTextBox1.Modified == true)
+            {
+                
+                label1.Text = string.Format("Кол-во символов: {0}", richTextBox1.Text.Length.ToString().Split(' ', '\n', '\t'));
+
+                int count = richTextBox1.Text.Split(' ', '\n', '\t').Length;
+                
+                label2.Text = string.Format("Кол-во слов: {0}",Convert.ToString(count));
+
+                string wordWithMaxLength = string.Empty;
+                string[] words = richTextBox1.Text.Split(' ','\n', '\t');
+                for (int i = 0; i < words.Length; i++)
+                    if (words[i].Length > wordWithMaxLength.Length)
+                        wordWithMaxLength = words[i];
+               
+                label3.Text = string.Format(wordWithMaxLength);
+
+                
+                int[] words_freq = new int[words.Length];           //это массив из целых чисел. индекс массива это номер слова в тексте. пока пустой
+                for (int i = 0; i < words.Length; i++)              //цикл с количеством пробегов, равным числу слов в тексте
+                {
+                    if (words_freq[i] >= 0)                         //если число повторений слова неотрицательно, то... (объяснение отрицательных значений см ниже)
+                    {
+                        words_freq[i] = 1;                          //ставим 1, потому что слово новое и повторяется минимум 1 раз
+                        for (int j = i + 1; j < words.Length; j++)  //сравниваем слово с каждым следующим словом в тексте
+                        {
+                            if (words[i] == words[j])               //если слова одинаковые, то
+                            {
+                                words_freq[i]++;                    //счетчик повторений увеличивает число на 1
+                                words_freq[j] = -99;                //слово-повтор учитывать в дальнейшем не нужно, для исключения из учета ему присваивается
+                            }                                       //отрицательное значение
+                        }
+                    }
+                }
+                int[] words_top = new int[10];                      //это массив, содержащий в себе топ-10 повторений (само их количество)
+                string[] words_toplist = new string[10];            //это массив, содержащий в себе слова из топ-10 (соотносится с words_top)
+                int temp = int.MaxValue;
+                bool flag = true;
+                for (int i = 0; i < 10; i++)                        //заходим в цикл 10 раз для выявления 10 самых частых слов
+                {
+                    for (int j = 0; j < words_freq.Length; j++)     //каждый раз пробегаем полное количество слов
+                    {
+                        if (words_freq[j] > words_top[i] && words_freq[j] <= temp)           //если слово по частоте попадается больше, чем то, которое в топе уже сидит, то
+                        {
+                            if (i == 0 && words[j] != "")                             //для первого слова просто
+                            {
+                                words_top[i] = words_freq[j];       //ставим новое наибольшее число в words_top и
+                                words_toplist[i] = words[j];        //ставим соответствующее слово в toplist
+                            }
+                            else
+                            {                                       //для всех последующих слов
+                                for (int k = 0; k < i; k++)         //в этом цикле проверяем, чтобы слова не совпадали, чтобы не поместить одно и то же слово дважды
+                                {
+                                    if (words[j] == words_toplist[k])   //если слова совпали, то
+                                    {
+                                        flag = false;                   //флаг становится ложью и
+                                        continue;                       //цикл заканчивается
+                                    }
+                                }
+                                if (flag == true && words[j] != "")                       //если флаг остался правдой, то
+                                {
+                                    words_top[i] = words_freq[j];       //ставим следующее по списку наибольшее число в words_top и
+                                    words_toplist[i] = words[j];        //ставим соответствующее слово в toplist
+                                }
+                            }
+                            flag = true;
+                        }
+                    }
+                    temp = words_top[i];
+
+                }
+                label4.Text = string.Format("Слово {0} встречается {1} раз\nСлово {2} встречается {3} раз\nСлово {4} встречается {5} раз\nСлово {6} встречается {7} раз\nСлово {8} встречается {9} раз\nСлово {10} встречается {11} раз\nСлово {12} встречается {13} раз\nСлово {14} встречается {15} раз\nСлово {16} встречается {17} раз\nСлово {18} встречается {19} раз", words_toplist[0], words_top[0], words_toplist[1], words_top[1], words_toplist[2], words_top[2], words_toplist[3], words_top[3], words_toplist[4], words_top[4], words_toplist[5], words_top[5], words_toplist[6], words_top[6], words_toplist[7], words_top[7], words_toplist[8], words_top[8], words_toplist[9], words_top[9]);
+
+
+                /*
+                string all_text = string.Join("", chars);
+                char[] alltext = all_text.ToCharArray();
+                
+                
+                for (int x = 0; x < alltext.Length; x++)
+                {
+                    if (chr[0] == alltext[x])
+                    {
+                        
+                    }
+                }*/
+                /*string[] chars = richTextBox1.Text.Length.ToString().Split(' ', '\n', '\t');
+                int char_count = 0;
+                string ch = textBox1.Text;
+                char[] chr = ch.ToCharArray();
+                for (int i = 0; i < richTextBox1.Lines.Length; i++)
+                {
+                    if (richTextBox1.Lines[i].Contains(ch)) // проверяем содержится символ в данной строке
+                    {
+                        char_count++;
+                    }
+                }
+                double percent = 100 * char_count / chars.Length;
+                // if (chars==textBox1.Text)
+                //textBox1.Text
+                label5.Text = string.Format("Символ {0} составляет {1}% от текста", ch, Convert.ToString(percent));*/
+
+
+            }
+            
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+           
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string str = richTextBox1.Text;
+            if (str.IndexOf(textBox1.Text) != -1)
+            {
+                
+                richTextBox1.SelectionStart = str.IndexOf(textBox1.Text);
+                richTextBox1.SelectionLength = textBox1.Text.Length;
+                richTextBox1.SelectionBackColor = Color.Red;
+                
+            }
+            else
+            {
+                MessageBox.Show("Не обнаружено!");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            richTextBox1.SelectionBackColor = Color.Transparent;
+        }
+    }
+}
